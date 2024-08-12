c206959155ce2d60b7b939ff8d614457f3f42f18
Vaishnavi-sivaraman <140157543+Vaishnavi-sivaraman@users.noreply.github.com>
2024-08-12 23:31:29 +0530

    front end-react

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#3490dc',
+        headingColor: '#1a202c',
+        textColor: '#333333'
+      },
+    },
+  },
+  plugins: [],
+}
+ 
+
